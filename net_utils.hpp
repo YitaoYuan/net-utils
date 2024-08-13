35a7549a4c550eb47eb2e8d7b6aa16649180f74c
YitaoYuan <yuanyitao@pku.edu.cn>
2024-08-14 00:33:19 +0800

    Update net_utils.hpp

--- net_utils.hpp
+++ net_utils.hpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <tuple>
 
 #include <unistd.h>
 #include <linux/ethtool.h>
@@ -24,33 +25,6 @@ using std::vector;
 
 #ifdef ENABLE_IB_UTILS
 #include <infiniband/verbs.h>
-
-static void show_devices() {
-    ibv_device **dev = ibv_get_device_list(NULL);
-    if(dev == NULL) {perror("ibv_get_device_list"); return;}
-    printf("Devices:\n");
-    for(int id = 0; dev[id]; id++) 
-        printf("%s\n", ibv_get_device_name(dev[id]));
-    ibv_free_device_list(dev);
-}
-
-static ibv_device *find_device(string name) {
-    ibv_device **dev = ibv_get_device_list(NULL);
-    ibv_device *ret = NULL;
-    int id;
-    if(dev == NULL) {
-        perror("ibv_get_device_list");
-        goto free_list;
-    }
-    for(id = 0; dev[id]; id++) 
-        if(!strcmp(ibv_get_device_name(dev[id]), name.c_str())) {
-            ret = dev[id];
-            break;
-        }
-free_list:
-    ibv_free_device_list(dev);
-    return ret;
-} 
 
 static union ibv_gid ipv4_to_gid(uint32_t ip)
 {
@@ -79,6 +53,76 @@ static string gid_to_str(ibv_gid gid)
     return s;
 }
 
+static void show_ib_devices() {
+    ibv_device **dev = ibv_get_device_list(NULL);
+    if(dev == NULL) {perror("ibv_get_device_list"); return;}
+    printf("Devices:\n");
+    for(int id = 0; dev[id]; id++) 
+        printf("%s\n", ibv_get_device_name(dev[id]));
+    ibv_free_device_list(dev);
+}
+
+bool get_gid_index(uint32_t ip, ibv_context *ctx, uint8_t *port_id, uint8_t *gid_index)
+{
+    ibv_device_attr dev_attr;
+    ibv_port_attr port_attr;
+    int ret = ibv_query_device(ctx, &dev_attr);
+    if(ret) {printf("Query device failed\n"); return false;}
+    for(uint8_t i = 1; i <= dev_attr.phys_port_cnt; i++) {
+        ret = ibv_query_port(ctx, i, &port_attr);
+        if(ret) {printf("Query port %d failed\n", (int)i); continue;}
+        for(int j = 1; j < port_attr.gid_tbl_len; j += 2) {// only query RoCEv2
+            ibv_gid gid;
+            ret = ibv_query_gid(ctx, i, j, &gid);
+            if(gid_to_ipv4(gid) != ip) continue;
+            *port_id = i;
+            *gid_index = j;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool query_ib_device_by_ip(uint32_t ip, ibv_device **dev, uint8_t *port_id, uint8_t *gid_index)
+{
+    ibv_device **dev_list = ibv_get_device_list(NULL);
+    bool found = false;
+    for(int id = 0; dev_list[id] && !found; id++) {
+        ibv_context *ctx = ibv_open_device(dev_list[id]);
+        if(ctx == NULL) {
+            printf("Open device %d failed\n", id);
+            continue;
+        }
+        if(get_gid_index(ip, ctx, port_id, gid_index)) {
+            *dev = dev_list[id];
+            found = true;
+        }
+        if(ibv_close_device(ctx) != 0) {
+            printf("Close device %d failed\n", id);
+        }
+    }
+    ibv_free_device_list(dev_list);
+    return found;
+}
+
+static ibv_device *find_ib_device(string name) {
+    ibv_device **dev = ibv_get_device_list(NULL);
+    ibv_device *ret = NULL;
+    int id;
+    if(dev == NULL) {
+        perror("ibv_get_device_list");
+        goto free_list;
+    }
+    for(id = 0; dev[id]; id++) 
+        if(!strcmp(ibv_get_device_name(dev[id]), name.c_str())) {
+            ret = dev[id];
+            break;
+        }
+free_list:
+    ibv_free_device_list(dev);
+    return ret;
+} 
+
 // input ens10f1
 // output mlx5_1
 static string dev_to_ib_dev(string dev)
@@ -106,27 +150,66 @@ static string ib_dev_to_dev(string ib_dev)
 }
 #endif
 
-// input 192.168.1.1
-// output ens10f1
-static string get_dev_by_ip(string ip_addr)
+static std::tuple<string, uint32_t> get_device_by_ip(uint32_t ip_addr, uint32_t prefix_mask = (uint32_t)-1)
 {
-    struct in_addr addr;
-    if(inet_aton(ip_addr.c_str(), &addr) == 0)
-        return "";
+    // struct in_addr addr;
+    // addr.s_addr = htonl(ip_addr);
     struct ifaddrs* if_list;
     if (getifaddrs(&if_list) < 0)
-        return "";
+        return std::tuple<string, uint32_t>{"", 0};
     string nic_name;
+    uint32_t nic_ip_addr;
     for (struct ifaddrs *ifa = if_list; ifa != NULL; ifa = ifa->ifa_next) {
         if (ifa->ifa_addr->sa_family == AF_INET) {
-            if (!memcmp(&addr, &(((struct sockaddr_in *) ifa->ifa_addr)->sin_addr), sizeof(struct in_addr))) {
+            struct in_addr nic_addr = ((struct sockaddr_in *) ifa->ifa_addr)->sin_addr;
+            nic_ip_addr = ntohl(nic_addr.s_addr);
+            if((ip_addr & prefix_mask) == (nic_ip_addr & prefix_mask)) {
                 nic_name = ifa->ifa_name;
                 break;
             }
+            // if (!memcmp(&addr, &(((struct sockaddr_in *) ifa->ifa_addr)->sin_addr), sizeof(struct in_addr))) {
+            //     nic_name = ifa->ifa_name;
+            //     break;
+            // }
         }
     }
     freeifaddrs(if_list);
-    return nic_name;
+    return std::tuple<string, uint32_t>{nic_name, nic_ip_addr};
+}
+
+// input 192.168.1.0/24 or 192.168.1.1
+// output ens10f1
+static std::tuple<string, uint32_t> get_device_by_ip(string ip_addr)
+{
+    for(size_t i = 0; i < ip_addr.size(); i++) {
+        if(ip_addr[i] == '/') {
+            string ip_string = ip_addr.substr(0, i); // pos, cnt
+            string prefix_len_string(ip_addr.begin() + i + 1, ip_addr.end());
+            uint32_t ip = ntohl(inet_addr(ip_string.c_str()));
+            uint32_t prefix_len = stoi(prefix_len_string);
+            uint32_t prefix_mask = (uint32_t)-1 << (32 - prefix_len);
+            return get_device_by_ip(ip, prefix_mask);
+        }
+    }
+    uint32_t ip = ntohl(inet_addr(ip_addr.c_str()));
+    return get_device_by_ip(ip);
+    // struct in_addr addr;
+    // if(inet_aton(ip_addr.c_str(), &addr) == 0)
+    //     return "";
+    // struct ifaddrs* if_list;
+    // if (getifaddrs(&if_list) < 0)
+    //     return "";
+    // string nic_name;
+    // for (struct ifaddrs *ifa = if_list; ifa != NULL; ifa = ifa->ifa_next) {
+    //     if (ifa->ifa_addr->sa_family == AF_INET) {
+    //         if (!memcmp(&addr, &(((struct sockaddr_in *) ifa->ifa_addr)->sin_addr), sizeof(struct in_addr))) {
+    //             nic_name = ifa->ifa_name;
+    //             break;
+    //         }
+    //     }
+    // }
+    // freeifaddrs(if_list);
+    // return nic_name;
 }
 
 // input : ens10f1
